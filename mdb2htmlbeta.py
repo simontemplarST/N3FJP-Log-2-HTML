a35ccd4be99be5f1e8077cbaf3d42bebfc753df8
Cameron <cameron@cameronheard.com>
2023-06-10 19:43:31 -0500

    Updated beta
    More to come. I'm done for the day.

--- mdb2htmlbeta.py
+++ mdb2htmlbeta.py
@@ -1,110 +1,162 @@
+import sys
+import subprocess
+import win32gui
+import win32process
 import pandas as pd
 import pyodbc
 import os
-import configparser
-import warnings
-warnings.filterwarnings('ignore', 'pandas only supports SQLAlchemy')
+import threading
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
 
-# Read the configuration from the config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
+# Hide the command prompt window
+def hide_console_window():
+    window = win32console.GetConsoleWindow()
+    win32gui.ShowWindow(window, 0)
+    _, pid = win32process.GetWindowThreadProcessId(window)
+    subprocess.call(['taskkill', '/F', '/PID', str(pid)], creationflags=subprocess.CREATE_NO_WINDOW)
 
-# Get the values from the configuration
-page_title = config.get('general', 'page_title')
-database_path = config.get('general', 'database_path')
-output_dir = config.get('general', 'output_dir')
-output_filename = config.get('general', 'output_filename')
-stylesheet = config.get('general', 'stylesheet')
-
-fldBand = config.get('columns', 'fldBand')
-fldCall = config.get('columns', 'fldCall')
-fldMode = config.get('columns', 'fldMode')
-fldRstR = config.get('columns', 'fldRstR')
-fldRstS = config.get('columns', 'fldRstS')
-fldSPCNum = config.get('columns', 'fldSPCNum')
-fldDateStr = config.get('columns', 'fldDateStr')
-fldTimeOnStr = config.get('columns', 'fldTimeOnStr')
-
-# Convert the database path and output directory to the appropriate format
-database_path = os.path.normpath(database_path)
-output_dir = os.path.normpath(output_dir)
-
-
-# Connect to the Access database using DSN
+# Connect to the Access database
 conn_str = (
     r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
     r'DBQ=Y:\LogData.mdb;'
 )
 
+def generate_html(output_file):
+    cnxn = pyodbc.connect(conn_str)
+    sql = "SELECT fldBand, fldCall, fldMode, fldRstR, fldRstS, fldSPCNum, fldDateStr, fldTimeOnStr FROM tblContacts ORDER BY fldDateStr DESC, fldTimeOnStr DESC"
+    df = pd.read_sql_query(sql, cnxn)
 
-cnxn = pyodbc.connect(conn_str)
+    # Rename the columns
+    df = df.rename(columns={
+        'fldBand': 'Band',
+        'fldCall': 'Call',
+        'fldMode': 'Mode',
+        'fldRstR': 'S',
+        'fldRstS': 'R',
+        'fldSPCNum': 'Country',
+        'fldDateStr': 'Date',
+        'fldTimeOnStr': 'Time'
+    })
 
-# Form the SQL query
-columns = ', '.join(config["columns"])
-sql = f"SELECT {columns} FROM tblContacts ORDER BY fldDateStr DESC, fldTimeOnStr DESC"
+    # Split the dataframe into two parts: top 10 rows and the rest
+    df_top10 = df.head(10)
+    df_rest = df.tail(len(df) - 10)
 
-df = pd.read_sql_query(sql, cnxn)
+    # Generate the HTML tables
+    html_table_top10 = df_top10.to_html(classes='top10', index=False)
+    html_table_rest = df_rest.to_html(classes='rest', index=False)
 
-# Rename the columns according to config
-df.rename(columns=config["columns"], inplace=True)
+    # Create the HTML page
+    css_file = css_var.get()
+    css_path = os.path.join('CSS', css_file)
+    css_link = f'    <link rel="stylesheet" type="text/css" href="{css_path}">\n'
 
-# Split the dataframe into two parts: top 10 rows and the rest
-df_top10 = df.head(10)
-df_rest = df.tail(len(df) - 10)
+    html_page = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>N0YEP's Log</title>
+        {css_link}
+        <script>
+            function searchTable() {{
+                var input, filter, table, tr, td, i, txtValue;
+                input = document.getElementById("searchInput");
+                filter = input.value.toUpperCase();
+                table = document.getElementsByClassName("top10")[0];
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {{
+                    td = tr[i].getElementsByTagName("td")[1]; // column index for "Call"
+                    if (td) {{
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {{
+                            tr[i].style.display = "";
+                        }} else {{
+                            tr[i].style.display = "none";
+                        }}
+                    }}
+                }}
+                table = document.getElementsByClassName("rest")[0];
+                tr = table.getElementsByTagName("tr");
+                for (i = 0; i < tr.length; i++) {{
+                    td = tr[i].getElementsByTagName("td")[1]; // column index for "Call"
+                    if (td) {{
+                        txtValue = td.textContent || td.innerText;
+                        if (txtValue.toUpperCase().indexOf(filter) > -1) {{
+                            tr[i].style.display = "";
+                        }} else {{
+                            tr[i].style.display = "none";
+                        }}
+                    }}
+                }}
+            }}
+        </script>
+    </head>
+    <body>
+        <center><h1>N0YEP's Log</h1></center>
+        <center><input type="text" id="searchInput" onkeyup="searchTable()" placeholder="Search for callsigns.."><center>
+        {html_table_top10}
+        {html_table_rest}
+    </body>
+    </html>
+    """
 
-# Generate the HTML tables
-html_table_top10 = df_top10.to_html(classes='top10', index=False)
-html_table_rest = df_rest.to_html(classes='rest', index=False)
+    with open(output_file, 'w') as f:
+        f.write(html_page)
 
+def run_script():
+    output_file = file_path.get()
+    generate_html(output_file)
 
-# Generate the HTML page snippet including the search script
-html = (
-    "<!DOCTYPE html>\n"
-    "<html>\n"
-    "<head>\n"
-    "  <meta charset=\"UTF-8\">\n"
-    f"  <title>{page_title}</title>\n"
-    f'  <link rel="stylesheet" type="text/css" href="{stylesheet}">\n'
-    '  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>\n'
-    "  <style>\n"
-    "    /* Add your custom CSS styles here */\n"
-    "  </style>\n"
-    "</head>\n"
-    "<body>\n"
-    f'  <h1>{page_title}</h1>\n'
-    '  <input type="text" id="searchInput" placeholder="Search by Callsign">\n'
-    "  <table>\n"
-    "    <thead>\n"
-    "      <tr>\n"
-    f"        <th>{config.get('columns', 'fldBand')}</th>\n"
-    f"        <th>{config.get('columns', 'fldCall')}</th>\n"
-    f"        <th>{config.get('columns', 'fldMode')}</th>\n"
-    f"        <th>{config.get('columns', 'fldRstR')}</th>\n"
-    f"        <th>{config.get('columns', 'fldRstS')}</th>\n"
-    f"        <th>{config.get('columns', 'fldSPCNum')}</th>\n"
-    f"        <th>{config.get('columns', 'fldDateStr')}</th>\n"
-    f"        <th>{config.get('columns', 'fldTimeOnStr')}</th>\n"
-    "      </tr>\n"
-    "    </thead>\n"
-    "    <tbody>\n"
-    "      <!-- Table rows will be dynamically generated here -->\n"
-    "    </tbody>\n"
-    "  </table>\n"
-    '  <script>\n'
-    "    $(document).ready(function() {\n"
-    "      $('#searchInput').on('keyup', function() {\n"
-    "        var value = $(this).val().toLowerCase();\n"
-    "        $('tbody tr').filter(function() {\n"
-    "          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);\n"
-    "        });\n"
-    "      });\n"
-    "    });\n"
-    "  </script>\n"
-    "</body>\n"
-    "</</html>\n"
-)
+    interval = int(time_var.get()) * 60 * 1000  # Convert minutes to milliseconds
+    threading.Timer(interval, run_script).start()  # Run the script at the specified interval
 
-# Save HTML to file
-output_file = os.path.join(config.get('general', 'output_dir'), config.get('general', 'output_filename'))
-with open(output_file, 'w') as f:
-    f.write(html)
+def select_output_file():
+    file_path.set(filedialog.asksaveasfilename(defaultextension='.html', filetypes=[('HTML Files', '*.html')]))
+
+# Hide the command prompt window
+if sys.argv[-1] != '-noconsole':
+    hide_console_window()
+
+# Create the GUI
+root = tk.Tk()
+root.title("Log to HTML Converter")
+
+file_path = tk.StringVar()
+css_var = tk.StringVar()
+time_var = tk.StringVar(value='10')  # Default interval value
+
+# Output File
+output_file_label = ttk.Label(root, text="Output File:")
+output_file_label.grid(row=0, column=0, sticky="w")
+
+output_file_entry = ttk.Entry(root, textvariable=file_path)
+output_file_entry.grid(row=0, column=1)
+
+output_file_button = ttk.Button(root, text="Select", command=select_output_file)
+output_file_button.grid(row=0, column=2)
+
+# CSS Dropdown
+css_label = ttk.Label(root, text="CSS Style:")
+css_label.grid(row=1, column=0, sticky="w")
+
+css_dropdown = ttk.Combobox(root, textvariable=css_var, state="readonly")
+css_dropdown.grid(row=1, column=1)
+css_dropdown['values'] = os.listdir('CSS')
+css_dropdown.current(0)
+
+# Time Interval Dropdown
+time_label = ttk.Label(root, text="Interval (minutes):")
+time_label.grid(row=2, column=0, sticky="w")
+
+time_dropdown = ttk.Combobox(root, textvariable=time_var, state="readonly")
+time_dropdown.grid(row=2, column=1)
+time_dropdown['values'] = ('1', '5', '10', '15', '30', '60')
+time_dropdown.current(2)  # Set default interval to 10 minutes
+
+# Generate HTML Button
+generate_button = ttk.Button(root, text="Generate HTML", command=run_script)
+generate_button.grid(row=3, column=0, columnspan=3, pady=10)
+
+root.mainloop()
